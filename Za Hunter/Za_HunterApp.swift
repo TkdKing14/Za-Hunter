0c1e8b3606b0a0afa7fd7de37ea662b71aa43fd0
Carson Payne <carson.j.payne14@gmail.com>
2025-02-04 10:05:44 -0600

    Initial Commit

---
+++ Za Hunter/Za_HunterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Za_HunterApp.swift
+//  Za Hunter
+//
+//  Created by Carson Payne on 2/4/25.
+//
+
+import SwiftUI
+
+@main
+struct Za_HunterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
